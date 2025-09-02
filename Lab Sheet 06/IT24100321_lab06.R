f85101b46e8d2f72f7348b4066a0dfb694d71f90
IT24100321 <it24100321@my.sliit.lk>
2025-09-02 22:10:17 +0530

    Lab 06

---
+++ Lab Sheet 06/IT24100321_lab06.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\UsEr\\Desktop\\it24100321")
+
+#Q1)
+#part 1) Binomial distribution
+#part 2)
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+#Q2)
+#part 1) Number of calls per hour
+#part 2) Poisson distribution.
+#part 3) 
+dpois(15,12)
